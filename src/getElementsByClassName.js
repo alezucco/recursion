183edacb77af8fd412315218478ee9e0f726368c
Alejandro Z <alejandrozuccolillo@hotmail.com>
2014-09-09 06:08:07 -0700

    fixed JSOn strigify and getElementsByClass

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,31 @@
-// If life was easy, we could just do things the easy way:
+// If life was evasy, we could just do things the easy way:
 // var getElementsByClassName = function (className) {
 //   return document.getElementsByClassName(className);
 // };
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+
+  var hasClass = function(node, className) {
+    var classes = node.className.split(' ');
+    return classes.indexOf(className) !== -1;
+  }
+
+  var node = arguments[1];
+  if (node === undefined) {
+    node = document.body;
+  }
+  var results = [];
+
+
+  if (hasClass(node, className)) {
+    results.push(node);
+  }
+
+  for (var i = 0; i < node.children.length; i++) {
+    var child = node.children[i];
+    results = _.union(results, getElementsByClassName(className, child));
+  }
+
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,4 +4,37 @@
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  //
+  var stringifyArray = function(a) {
+    return '[' + _.map(a, stringifyJSON).join(',') + ']';
+  }
+
+  var stringifyObject = function(o) {
+
+    var pairings = [];
+
+    var stringifyPairing = function(v,k) {
+      if (v !== undefined && typeof(v) !== "function") {
+        pairings.push("\"" + k + "\"" + ':' + stringifyJSON(v));
+      }
+    }
+    _.each(o, stringifyPairing);
+    return '{' + pairings.join(',') + '}';
+  }
+
+  var t = typeof(obj);
+  if (t === "number" || t === "boolean") {
+    return obj+'';
+  } else if (t === "string") {
+    return "\"" + obj + "\"";
+  } else if (obj === undefined
+          || obj === null
+          || t === "function"
+          || obj === NaN) {
+    return "null";
+  } else if (Array.isArray(obj)) {
+    return stringifyArray(obj);
+  } else { // It's an Object!
+    return stringifyObject(obj);
+  }
 };
